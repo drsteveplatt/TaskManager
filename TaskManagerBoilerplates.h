2e54c55debbf6f2cde7f913b6cc363c8dcaeed17
Steve Platt <steveplatt@comcast.net>
2020-12-25 11:13:00 -0500

    Updated TM_MACROS: updated bradkets on TM_YIELD* code

---
+++ TaskManagerBoilerplates.h
@@ -0,0 +1,43 @@
+// TaskManager Boilerplates
+//
+
+// Various boilerplate utilities
+
+// Unblocking Read
+// Read from a stream into a String
+// This is useful in TM_Macro code, so it will need TM_BEGIN() and TM_END().
+//	stream_in is the stream to use (normally Serial)
+//	string_out is the string variable to place the results in
+//	yield_id is the TM_MACRO ID for TM_YIELD
+//	echo is a boolean -- if true, echo the characters back to stream_in as read in
+// TM_READLINE(stream_in, string_out, yield_id, echo)
+#define TM_READLINE(stream_in, string_out, yield_id, echo)			\
+  { /* read in a whole line of chars into cmd	*/					\
+    char ch;														\
+    bool done;														\
+    bool lastWasCr;													\
+    cmd = "";														\
+    done = false;													\
+    lastWasCr = false;												\
+    while(!done) {													\
+      while(!stream_in.available()) { TM_YIELD(yield_id); }			\
+      ch = stream_in.read();										\
+      /* Backspace processing for PuTTY	*/							\
+      if(ch==0x08 && echo) {										\
+        if(cmd.length()>0) {										\
+          Serial << '\b' << ' ' << '\b';							\
+          cmd.remove(cmd.length()-1);								\
+        }															\
+      } else {														\
+        /* CR processing for PuTTY */								\
+        if(ch=='\r') { ch='\n'; lastWasCr = true; }					\
+        else if(ch=='\n' && lastWasCr) { lastWasCr=false; continue; } /* crlf, skip the lf */		\
+        if(ch=='\n') { Serial << ch; lastWasCr = false; done = true; }								\
+        else {														\
+          if(echo)Serial << ch;										\
+          string_out += ch;											\
+          lastWasCr = false;										\
+        } /* end if(ch=='\n') else */								\
+      } /* end if(ch==0x08) else */									\
+    } /* end while */												\
+  } /* end read in a whole line of chars */
--- TaskManagerMacros.h
+++ TaskManagerMacros.h
@@ -55,10 +55,10 @@
 	\param n -- an integer label.  The label should be unique for all of the TM_YIELD*()
 	routines in this task.
 */
-#define TM_YIELD(n)							\
+#define TM_YIELD(n)	{						\
 			__tmNext__ = n;					\
 			TaskMgr.yield();				\
-		case n:
+		case n:  ;   }
 
 /*!	\brief	Yield with a delay and return to the next statement
 
@@ -68,10 +68,10 @@
 	routines in this task.
 	\param ms -- a long integer value representing the time (in ms) for the delay.
 */
-#define TM_YIELDDELAY(n,ms)					\
+#define TM_YIELDDELAY(n,ms)	{				\
 			__tmNext__ = n;					\
 			TaskMgr.yieldDelay(ms);			\
-		case n:
+		case n: ; }
 
 /*!	\brief	Yield until a signal is received, and then return to the next statement
 
@@ -81,10 +81,10 @@
 	routines in this task.
 	\param sigNum -- a byte value representing the signal the task will be waiting for.
 */
-#define TM_YIELDSIGNAL(n,sig)				\
+#define TM_YIELDSIGNAL(n,sig)	{			\
 			__tmNext__ = n;					\
 			TaskMgr.yieldForSignal(sig);	\
-		case n:
+		case n: ; }
 
 /*!	\brief	Yield until a message has been received, and then return to the next statement
 
@@ -95,10 +95,10 @@
 */
 
 
-#define TM_YIELDMESSAGE(n)					\
+#define TM_YIELDMESSAGE(n)	{				\
 			__tmNext__ = n;					\
 			TaskMgr.yieldForMessage();		\
-		case n:
+		case n: ; }
 
 /*!	\brief	Yield until a signal is received or a time has passed, and then return to the next statement
 
@@ -109,10 +109,10 @@
 	\param sigNum -- a byte value representing the signal the task will be waiting for.
 	\param msTimeout -- the maximal amount of time the task will wait for the signal.
 */
-#define TM_YIELDSIGNALTIMEOUT(n,sig,msto)	\
-			__tmNext__ = n;					\
+#define TM_YIELDSIGNALTIMEOUT(n,sig,msto)	{   \
+			__tmNext__ = n;					    \
 			TaskMgr.yieldForSignal(sig,msto);	\
-		case n:
+		case n: ; }
 
 /*!	\brief	Yield until a message is received or a time has passed, and then return to the next statement
 
@@ -122,11 +122,52 @@
 	routines in this task.
 	\param msTimeout -- the maximal amount of time the task will wait for the signal.
 */
-#define TM_YIELDMESSAGETIMEOUT(n,msto)		\
+#define TM_YIELDMESSAGETIMEOUT(n,msto)	{	\
 			__tmNext__ = n;					\
 			TaskMgr.yieldForMessage(msto);	\
-		case n:
+		case n:  ; }
 
 /*! @} */
 
+/*!	@name Task Manager Callable Subtasks
+	These macros allow for the definition of a TM subtask -- a procedure
+	that can be called from task.
+
+	Each subtask is identified by a unique ID.  This ID is both a signal ID and a task ID.
+	It should not be used by any other subtask or for any other signalling purposes
+*/
+/*! @{ */
+
+/*!	\brief	Define a callable subtask
+	TM_ADD_SUBTASK() defines the task and prepares it to wait for activation.
+	It should be used instead of any of the TaskManager.add*() routines.
+	Note that the 'void subtask(){...}' will need to be defined elsewhere.
+*/
+#define TM_ADD_SUBTASK(id, task) TaskMgr.addAutoWaitSignal(id, task, id);
+
+/*!	\brief Procedure definition header for subtask
+	TM_BEGINSUB(id) is used at the start of a subtask procedure
+	\param id -
+*/
+#define TM_BEGINSUB(id)	TM_BEGIN()
+
+/*!	\brief Return from a subtask
+	TM_RETURNSUB() is used to return from a procedure.  Note that all subtasks MUST use
+	TM_RETURNSUB() -- bare returns are not allowed.  TM_RETURNSUB() can be used
+	from multiple places within a subtask
+*/
+#define TM_RETURNSUB(id)  { TaskMgr.sendSignal(0, id); return; }
+
+/*!	\brief Call a subtask
+	TM_CALL() calls a subtask.  When the subtask has been completed (via TM_SUBTASK_RETURN()), the
+	calling routine will resume.
+*/
+#define TM_CALL(localId, taskId) { TaskMgr.sendSignal(0, taskId); TM_YIELDSIGNAL(localId, taskId); }
+
+
+/*! \brief End a subtask
+	TM_ENDSUB(parentId)
+	Used at the bottom of a subtask.
+*/
+/*! @} */
 #endif
